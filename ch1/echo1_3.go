4c0528713e2354e0a67cb6a4788ff273e195d2b1
Andrii Guselietov <guseletov@gmail.com>
2019-09-13 16:55:42 +0200

    Ex 1.3 with elapsing time

--- ch1/echo1_3.go
+++ ch1/echo1_3.go
@@ -5,16 +5,23 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	// variant 1
+	fmt.Println("Starting variant 1 (for loop)")
+	start := time.Now()
 	s, sep := "", ""
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
+	fmt.Printf("End of run for variant 1 : %dMs elapsed \n", time.Since(start).Microseconds())
 	// variant 2
+	fmt.Println("Starting variant 2 (Stings.join)")
+	start = time.Now()
 	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Printf("End of run for variant 2 : %dMs elapsed \n", time.Since(start).Microseconds())
 }
